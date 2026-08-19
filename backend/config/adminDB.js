dbccd411ff878531773b88ca0615c22b200be7de
Nikhil Patil <nikhil.patil@jayshreepolymers.com.com>
2026-08-19 18:01:24 +0530

    new changes

--- backend/config/adminDB.js
+++ backend/config/adminDB.js
@@ -11,14 +11,17 @@ const adminDB = mysql.createPool({
     queueLimit: 0
 });
 
+
+
 // const adminDB = mysql.createPool({
-//     host: "127.0.0.1",
-//     user: "jayashreeadmin_jayashreeadmin",
-//     password: "pyuP5-S]$]Aci1~K",
-//     database: "jayashreeadmin_jayashreeadmin",
-//     waitForConnections: true,
-//     connectionLimit: 10,
-//     queueLimit: 0
+//   host: "127.0.0.1",
+//   user: "jayashreeadmin_jayashreeadmin",
+//   password: "pyuP5-S]$]Aci1~K",
+//   database: "jayashreeadmin_jayashreeadmin",
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0,
 // });
+
 
 export default adminDB;
