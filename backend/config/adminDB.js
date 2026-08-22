c271f948cdb5b8f84f58cc4911e5b9c5a129a985
Nikhil Patil <nikhil.patil@jayshreepolymers.com.com>
2026-08-22 16:20:39 +0530

    env

--- backend/config/adminDB.js
+++ backend/config/adminDB.js
@@ -1,28 +1,28 @@
 import mysql from "mysql2/promise";
 
 
-// const adminDB = mysql.createPool({
-//     host: "127.0.0.1",
-//     user: "root",
-//     password: "",
-//     database: "admin",
-//     waitForConnections: true,
-//     connectionLimit: 10,
-//     queueLimit: 0
-// });
-// export default adminDB;
-
-
 const adminDB = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: Number(process.env.DB_PORT || 3306),
-
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
+    host: "127.0.0.1",
+    user: "root",
+    password: "",
+    database: "admin",
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
-
 export default adminDB;
+
+
+// const adminDB = mysql.createPool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+//   port: Number(process.env.DB_PORT || 3306),
+
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0,
+// });
+
+// export default adminDB;
